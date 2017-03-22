dfd31991895c870a33a0c324979d2ee93d11f94f
Michal Vinkler <mvinkler@redhat.com>
2017-03-22 13:04:33 +0100

    add clustered ejb client mvn project

---
+++ ejbclient/eap7.1-clustered-jboss-ejb-client-properties/client/src/main/java/client/Client.java
@@ -0,0 +1,71 @@
+package client;
+
+import java.security.PrivilegedActionException;
+import java.security.Provider;
+import java.util.Hashtable;
+import java.util.Properties;
+import java.util.Scanner;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.jboss.test.clusterbench.ejb.stateful.RemoteStatefulSB;
+import org.jboss.test.clusterbench.ejb.stateful.RemoteStatefulSBImpl;
+import org.wildfly.security.WildFlyElytronProvider;
+import org.wildfly.security.auth.client.AuthenticationConfiguration;
+import org.wildfly.security.auth.client.AuthenticationContext;
+import org.wildfly.security.auth.client.MatchRule;
+
+/**
+ * @author mvinkler
+ */
+public class Client {
+
+    public static void main(String[] args) throws NamingException, PrivilegedActionException {
+
+        Hashtable jndiProperties = new Hashtable();
+        jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+
+
+        AuthenticationConfiguration common = AuthenticationConfiguration.EMPTY
+                .useProviders(() -> new Provider[] {new WildFlyElytronProvider()})
+                .allowAllSaslMechanisms();
+        AuthenticationContext authCtxEmpty = AuthenticationContext.empty();
+        final AuthenticationContext authCtx = authCtxEmpty.with(MatchRule.ALL, common);
+        AuthenticationContext.getContextManager().setThreadDefault(authCtx);
+
+        Context context = new InitialContext(jndiProperties);
+
+        String beanName = RemoteStatefulSBImpl.class.getSimpleName();
+        String viewClassName = RemoteStatefulSB.class.getName();
+
+        RemoteStatefulSB remoteStatefulSB = (RemoteStatefulSB) context.lookup("ejb:clusterbench-ee7/clusterbench-ee7-ejb//" + beanName + "!" + viewClassName + "?stateful");
+
+        System.out.println("remoteStatefulSB: " + remoteStatefulSB.toString());
+
+        promptEnterKey(remoteStatefulSB);
+
+        context.close();
+
+    }
+
+    private static void promptEnterKey(RemoteStatefulSB remoteStatefulSB) {
+
+        while(true) {
+
+            System.out.println("write \"exit\" to exit, pres \"ENTER\" to continue...");
+            Scanner scanner = new Scanner(System.in);
+            String s = scanner.nextLine();
+
+            if (s.startsWith("exit")) {
+                return;
+            }
+
+            System.out.println(remoteStatefulSB.getSerialAndIncrement());
+
+        }
+    }
+
+
+
+}
